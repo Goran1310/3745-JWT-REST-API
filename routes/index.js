231e9d8bd46455543d62a552b0b702c17e670f46
Goran Lovincic <lovincic@gmail.com>
2023-05-26 12:02:52 +0200

    Adding login.ejs, signup.ejs and change index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const users = [];
+let id = 1;
+
+// Render the home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// Render the signup page
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+// Render the login page
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+// Handle post request for login
+router.post("/login", async (req, res, next) => {
+  let { email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = users.find(element => element.email == email);
+  } catch {
+    const error = new Error("Error! Something went wrong.");
+    return next(error);
+  }
+  if (!existingUser || existingUser.password != password) {
+    const error = Error("Wrong details please check at once");
+    return next(error);
+  }
+  res.status(200).json({
+    success: true,
+    data: {
+      userId: existingUser.id,
+      email: existingUser.email,
+    },
+  });
+});
+
+// Handle post request for signup
+router.post("/signup", async (req, res, next) => {
+  const { email, password } = req.body;
+  const newUser = {
+    id,
+    email,
+    password,
+  };
+  users.push(newUser);
+  id++;
+  res.status(201).json({
+    success: true,
+    data: {
+      userId: newUser.id,
+      email: newUser.email,
+    },
+  });
 });
 
 module.exports = router;
